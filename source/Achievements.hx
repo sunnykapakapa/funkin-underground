db7df628bbe60ad79f682c7b3e641c29fa9bceca
sunnykapakapa <94994891+sunnykapakapa@users.noreply.github.com>
2022-07-10 17:27:19 -0300

    Update Achievements.hx

--- source/Achievements.hx
+++ source/Achievements.hx
@@ -27,6 +27,8 @@ class Achievements {
 		["Just the Two of Us",			"Finish a Song pressing only two keys.",			'two_keys',				false],
 		["Toaster Gamer",				"Have you tried to run the game on a toaster?",		'toastie',				false],
 		["Debugger",					"Beat the \"Test\" Stage from the Chart Editor.",	'debugger',				 true]
+		["Interesting",					"Beat Gaster on Hard with no Misses",	'gasternomisses',				 false]
+
 	];
 	public static var achievementsMap:Map<String, Bool> = new Map<String, Bool>();
 
